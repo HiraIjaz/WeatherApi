07d6450735c2a0cb56e3cd67de28f8c5d2aeb907
hiraijaz <hiraijaz956@gmail.com>
2022-10-05 20:27:22 +0500

    edited indentation

--- weatherReport/metar/views.py
+++ weatherReport/metar/views.py
@@ -23,19 +23,19 @@ def getweatherinfo(request, scode):
     data_list = result.text.split()
     raw_temprarture = data_list[8].split('/')
     if raw_temprarture[0][0] == 'M':
-        temp_celcius = '-' + raw_temprarture[0] + 'C'
+        temp_celcius = '-' + raw_temprarture[0] + ' C'
     else:
-        temp_celcius = raw_temprarture[0] + 'C'
+        temp_celcius = raw_temprarture[0] + ' C'
     if raw_temprarture[1][0] == 'M':
-        temp_fahrenheit = '-' + raw_temprarture[1] + 'F'
+        temp_fahrenheit = '-' + raw_temprarture[1] + ' F'
     else:
-        temp_fahrenheit = raw_temprarture[1] + 'F'
+        temp_fahrenheit = raw_temprarture[1] + ' F'
 
     data_dict = {
         'station': data_list[2],
         'last_observation': data_list[0] + ' at ' + data_list[1] \
             + ' GMT',
-        'temperature': temp_celcius + ' ( ' + temp_fahrenheit + ')',
+        'temperature': temp_celcius + ' (' + temp_fahrenheit + ')',
         'wind': data_list[3],
         }
 
